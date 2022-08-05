46ab648e15de72832a5621ca4ef040f0f5c2c160
iwan <iwancuah13@gmail.com>
2022-08-05 14:31:02 +0700

    responsive login

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
 ?>
 <html>
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="shortcut icon" href="images/icon/favicon.ico">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<style>
